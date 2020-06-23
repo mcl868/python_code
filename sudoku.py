4b2533251bcc19cf3e2279c2508d3d5e5e7de294
Thomas Maltesen <atmaltesen@gmail.com>
2020-06-23 15:52:19 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -3,7 +3,7 @@ import numpy
 
 
 
-def checkrow(a):
+def check(a):
     numbers = numpy.arange(1,len(a)+1)
     s = []
     for i in range(len(a)):
@@ -29,7 +29,6 @@ def feild(a):
             place.append(i)
     return place
 
-
 def checkall(x):
     vec = numpy.array(x).reshape(-1)
     numbers = numpy.arange(1,len(vec)+1)
@@ -37,8 +36,8 @@ def checkall(x):
     res = all(numbers == numpy.array(sorted(vec)))
     return res
 
-
-
+def intersection(list1, list2): 
+    return list(set(list1) & set(list2))
 
 def board(x):
     for j in range(9):
@@ -54,29 +53,76 @@ def board(x):
 
 
 
-line1 = [0,0,4,6,7,8,9,0,2]
-line2 = [6,7,2,1,9,5,3,4,0]
-line3 = [1,9,8,3,4,2,5,6,7]
+def solvesoduku(x):
+    solved = True
+    aa = x
+    while solved:
+        for j in range(9):
+            set1pre = aa[j,0:9]
+            set1 = check(set1pre)
+            for i in range(9):
+                set2pre = aa[0:9,i]
+                set2 = check(set2pre)
+                if intersection(set1,set2) and aa[j,i] == 0:
+                    aa[j,i] = numpy.sum(intersection(set1,set2))
+        solved = (not (checkall(aa[0:9,0]) and
+                       checkall(aa[0:9,1]) and
+                       checkall(aa[0:9,2]) and
+                       checkall(aa[0:9,3]) and
+                       checkall(aa[0:9,4]) and
+                       checkall(aa[0:9,5]) and
+                       checkall(aa[0:9,6]) and
+                       checkall(aa[0:9,7]) and
+                       checkall(aa[0:9,8])))
+    return board(aa)
+
+
+print("")
+print("               ____  _      _ ____     ____   _    __ _      _")
+print("              / ___ | |    | |  _ \   / __ \ | |  / /| |    | |")
+print("             | /    | |    | | | \ \ / /  \ \| |_/ / | |    | |")
+print("             | \___ | |    | | |  | | |    | |    /  | |    | |")
+print("              \___ \| |    | | |  | | |    | | |\ \  | |    | |")
+print("                  \ | |    | | |  | | |    | | | \ \ | |    | |")
+print("               ___/ /\ \__/ /| |_/ / \ \__/ /| |  \ \ \ \__/ /")
+print("              |____/  \____/ |____/   \____/ |_|   \_\ \____/")
+print("")
+print("")
+
+
+
+
+
+
+line1 = [0,3,4,6,7,8,9,1,0]
+line2 = [6,7,0,1,9,5,3,4,0]
+line3 = [0,9,8,3,4,0,5,6,7]
 line4 = [8,5,9,0,6,1,4,2,3]
-line5 = [4,0,6,8,5,3,7,9,1]
-line6 = [7,1,3,9,2,0,8,5,6]
-line7 = [9,6,1,0,3,7,2,8,4]
-line8 = [2,8,0,4,1,9,6,3,5]
-line9 = [0,4,5,2,8,6,1,7,9]
+line5 = [4,0,6,8,0,3,0,9,1]
+line6 = [0,1,3,9,2,0,8,5,6]
+line7 = [9,6,1,0,3,7,2,0,4]
+line8 = [2,0,0,4,1,9,6,0,5]
+line9 = [0,4,5,0,8,0,0,7,9]
 
 
 mab = numpy.array([line1,line2,line3,line4,line5,line6,line7,line8,line9])
 
 board(mab)
-
-print(checkrow(line1))
-
-print(checkfeild(mab[0:3,0:3]))
-
-
-print(all(checkfeild(mab[0:3,0:3]) in checkrow(line1)))
+print("")
+print("... and the solution is:")
+solvesoduku(mab)
 
 
+
+
+
+
+
+
+
+
+
+'''
 
 def solvesoduku(x):
     b=0
@@ -88,11 +134,13 @@ def solvesoduku(x):
         for j in range(9):
             row = aa[j]
 
-            aaaaaa = random.sample(checkrow(row),len(checkrow(row)))
+            cr = check(row)
+
+            aaaaaa = random.sample(cr,len(cr))
 
             f = numpy.array(feild(row))
 
-            for i in range(len(checkrow(row))):
+            for i in range(len(cr)):
                 row[f[i]] = aaaaaa[i]
 
             aav2.append(row)
@@ -131,87 +179,6 @@ def makeboard():
     return numpy.array(my_my_list)
 
 
-print("")
-print("               ____  _      _ ____     ____   _    __ _      _")
-print("              / ___ | |    | |  _ \   / __ \ | |  / /| |    | |")
-print("             | /    | |    | | | \ \ / /  \ \| |_/ / | |    | |")
-print("             | \___ | |    | | |  | | |    | |    /  | |    | |")
-print("              \___ \| |    | | |  | | |    | | |\ \  | |    | |")
-print("                  \ | |    | | |  | | |    | | | \ \ | |    | |")
-print("               ___/ /\ \__/ /| |_/ / \ \__/ /| |  \ \ \ \__/ /")
-print("              |____/  \____/ |____/   \____/ |_|   \_\ \____/")
-print("")
-print("")
 
-
-
-
-
-
-line1 = [5,3,4,6,7,8,9,1,0]
-line2 = [6,7,2,1,9,5,3,4,0]
-line3 = [0,9,8,3,4,2,5,6,7]
-line4 = [8,5,9,0,6,1,4,2,3]
-line5 = [4,0,6,8,5,3,7,9,1]
-line6 = [7,1,3,9,2,0,8,5,6]
-line7 = [9,6,1,0,3,7,2,8,4]
-line8 = [2,8,0,4,1,9,6,3,5]
-line9 = [0,4,5,2,8,6,1,7,9]
-
-
-mab = numpy.array([line1,line2,line3,line4,line5,line6,line7,line8,line9])
-
-board(mab)
-print("")
-print("... and the solution is:")
-solvesoduku(mab)
-
-
-'''
-
-mab=makeboard()
-
-print(board(mab))
-print(solvesoduku(mab))
-
-
-
-b=0
-solved = True
-while solved:
-    aa = numpy.array([line1,line2,line3,line4,line5,line6,line7,line8,line9])
-    b=b+1
-    aav2 = []
-    for j in range(9):
-        row = aa[j]
-
-        aaaaaa = random.sample(checkrow(row),len(checkrow(row)))
-
-        f = numpy.array(feild(row))
-
-        for i in range(len(checkrow(row))):
-            row[f[i]] = aaaaaa[i]
-
-        aav2.append(row)
-    aav3 = numpy.array(aav2)
-
-    solved = (not (checkall(aav3[0:3,0:3]) and
-           checkall(aav3[0:3,3:6]) and
-           checkall(aav3[0:3,6:9]) and
-           checkall(aav3[3:6,0:3]) and
-           checkall(aav3[3:6,3:6]) and
-           checkall(aav3[3:6,6:9]) and
-           checkall(aav3[6:9,0:3]) and
-           checkall(aav3[6:9,3:6]) and
-           checkall(aav3[6:9,6:9]) and
-           checkall(aav3[0:9,0]) and
-           checkall(aav3[0:9,1]) and
-           checkall(aav3[0:9,2]) and
-           checkall(aav3[0:9,3]) and
-           checkall(aav3[0:9,4]) and
-           checkall(aav3[0:9,5]) and
-           checkall(aav3[0:9,6]) and
-           checkall(aav3[0:9,7]) and
-           checkall(aav3[0:9,8])))
 
 '''
