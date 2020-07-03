f8b8f3518be46cf18573284a11390d89f2d6fcac
Thomas Maltesen <atmaltesen@gmail.com>
2020-07-03 14:29:15 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -9,35 +9,48 @@ def check(a):
             s.append(numbers[i])
     return s
 
-def checkfeild(x):
-    vec = numpy.array(x).reshape(-1)
-    numbers = numpy.arange(1,len(vec)+1)
-
-    s = []
-    for i in range(len(vec)):
-        if numbers[i] not in vec:
-            s.append(numbers[i])
-    return s
-
-def feild(a):
-    numbers = numpy.arange(1,len(a)+1)
-    place = []
-    for i in range(len(a)):
-        if a[i] == 0:
-            place.append(i)
-    return place
+#############################################
+##def checkfeild(x):                       ##
+##    vec = numpy.array(x).reshape(-1)     ##
+##    numbers = numpy.arange(1,len(vec)+1) ##
+##    s = []                               ##
+##    for i in range(len(vec)):            ##
+##        if numbers[i] not in vec:        ##
+##            s.append(numbers[i])         ##
+##    return s                             ##
+##def feild(a):                            ##
+##    numbers = numpy.arange(1,len(a)+1)   ##
+##    place = []                           ##
+##    for i in range(len(a)):              ##
+##        if a[i] == 0:                    ##
+##            place.append(i)              ##
+##    return place                         ##
+#############################################
 
 def checkall(x):
     vec = numpy.array(x).reshape(-1)
     numbers = numpy.arange(1,len(vec)+1)
-
     res = all(numbers == numpy.array(sorted(vec)))
     return res
 
-def intersection(list1, list2): 
-    return list(set(list1) & set(list2))
+def intersection(list1, list2):
+    inters = list(set(list1) & set(list2))
+    if len(inters) >1:
+        inters = []
+    return inters
 
-def board(x):
+def board2(x):
+    for j in range(4):
+       if j == 0:
+           print("                    ________ ________ ")
+       if j in numpy.array([2,4]):
+           print("                   |________|________|")
+       print("                   |        |        |")
+       print("                   |  {0}  {1}  |  {2}  {3}  |".
+             format(x[j,0],x[j,1],x[j,2],x[j,3]))
+    print("                   |________|________|")
+
+def board3(x):
     for j in range(9):
        if j == 0:
            print("                    ___________ ___________ ___________")
@@ -46,7 +59,6 @@ def board(x):
        print("                   |           |           |           |")
        print("                   |  {0}  {1}  {2}  |  {3}  {4}  {5}  |  {6}  {7}  {8}  |".
              format(x[j,0],x[j,1],x[j,2],x[j,3],x[j,4],x[j,5],x[j,6],x[j,7],x[j,8]))
-
     print("                   |___________|___________|___________|")
 
 def makeboard(a):
@@ -54,13 +66,14 @@ def makeboard(a):
     for l in range(a):
         list1 = [int(item) for item in input("Row "+str(l+1)+" : ").split()] 
         my_my_list.append(list1)
-
     return numpy.array(my_my_list)
 
-def solvesoduku(x):
+def solvesoduku(x, itera=300):
     solved = True
     dimx = numpy.array(x.shape)[0]
+
     aa = x
+    b = 0
     while solved:
         for j in range(dimx):
             set1pre = aa[j,0:dimx]
@@ -70,19 +83,27 @@ def solvesoduku(x):
                 set2 = check(set2pre)
                 if intersection(set1,set2) and aa[j,i] == 0:
                     aa[j,i] = numpy.sum(intersection(set1,set2))
-        solved = (not (checkall(aa[0:9,0]) and
-                       checkall(aa[0:9,1]) and
-                       checkall(aa[0:9,2]) and
-                       checkall(aa[0:9,3]) and
-                       checkall(aa[0:9,4]) and
-                       checkall(aa[0:9,5]) and
-                       checkall(aa[0:9,6]) and
-                       checkall(aa[0:9,7]) and
-                       checkall(aa[0:9,8])))
-    return board(aa)
+        sol = []
+        for k in range(dimx):
+             sol.append(checkall(aa[0:dimx,k]))
+        solved = (not all(sol))
+        b = b+1
+        if b>itera:
+            solved = False
+            print("")
+            print("Warning, exceed "+str(itera)+" iterations.")
+    if b<itera:       
+        print("")
+        print("... and the solution is:")
 
+    if numpy.sqrt(dimx) == 3:
+        rules = board3(aa)
+    if numpy.sqrt(dimx) == 2:
+        rules = board2(aa)
 
-def playsoduku(a):
+    return rules
+
+def playsoduku(a=9):
     print("")
     print("               ____  _      _ ____     ____   _    __ _      _")
     print("              / ___ | |    | |  _ \   / __ \ | |  / /| |    | |")
@@ -94,42 +115,50 @@ def playsoduku(a):
     print("              |____/  \____/ |____/   \____/ |_|   \_\ \____/")
     print("")
     print("")
-
     mab = makeboard(a)
-
-    board(mab)
-    print("")
-    print("... and the solution is:")
-
+    board3(mab)
     return solvesoduku(mab)
 
 '''
-line1 = [0 3 4 6 7 8 9 1 0]
-line2 = [6 7 0 1 9 5 3 4 0]
-line3 = [0 9 8 3 4 0 5 6 7]
-line4 = [8 5 9 0 6 1 4 2 3]
-line5 = [4 0 6 8 0 3 0 9 1]
-line6 = [0 1 3 9 2 0 8 5 6]
-line7 = [9 6 1 0 3 7 2 0 4]
-line8 = [2 0 0 4 1 9 6 0 5]
-line9 = [0 4 5 0 8 0 0 7 9]
+line1 = [5 3 0 0 7 0 0 0 0]
+line2 = [6 0 0 1 9 5 0 0 0]
+line3 = [0 9 8 0 0 0 0 6 0]
+line4 = [8 0 0 0 6 1 4 0 3]
+line5 = [4 0 6 0 0 3 0 0 1]
+line6 = [0 1 3 9 2 0 8 0 6]
+line7 = [0 6 0 0 0 0 2 8 0]
+line8 = [0 0 0 4 1 9 0 0 5]
+line9 = [0 0 0 0 8 0 0 7 9]
 
-line1 = [0,3,4,6,7,8,9,1,0]
-line2 = [6,7,0,1,9,5,3,4,0]
-line3 = [0,9,8,3,4,0,5,6,7]
-line4 = [8,5,9,0,6,1,4,2,3]
-line5 = [4,0,6,8,0,3,0,9,1]
-line6 = [0,1,3,9,2,0,8,5,6]
-line7 = [9,6,1,0,3,7,2,0,4]
-line8 = [2,0,0,4,1,9,6,0,5]
-line9 = [0,4,5,0,8,0,0,7,9]
 
-mab = numpy.array([line1,line2,line3,line4,line5,line6,line7,line8,line9])
 
-print(numpy.array(mab.shape)[0])
+line1 = [5,3,0,0,7,0,0,0,0]
+line2 = [6,0,0,1,9,5,0,0,0]
+line3 = [0,9,8,0,0,0,0,6,0]
+line4 = [8,0,0,0,6,1,4,0,3]
+line5 = [4,0,6,0,0,3,0,0,1]
+line6 = [0,1,3,9,2,0,8,0,6]
+line7 = [0,6,0,0,0,0,2,8,0]
+line8 = [0,0,0,4,1,9,0,0,5]
+line9 = [0,0,0,0,8,0,0,7,9]
 
-board(mab)
-print("")
-print("... and the solution is:")
-solvesoduku(mab)
+
+mab3 = numpy.array([line1,line2,line3,line4,line5,line6,line7,line8,line9])
+
+board3(mab3)
+
+solvesoduku(mab3)
+
+
+
+line1 = [0,2,3,4]
+line2 = [3,4,2,0]
+line3 = [4,0,1,2]
+line4 = [0,1,0,3]
+
+mab2 = numpy.array([line1,line2,line3,line4])
+
+board2(mab2)
+
+solvesoduku(mab2)
 '''
